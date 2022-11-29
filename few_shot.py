316d64dd00708b41575a1ea8d74067a17db50816
nrsmac <noahrschill@gmail.com>
2022-11-29 09:21:53 -0700

    modified few-shot

--- few_shot.py
+++ few_shot.py
@@ -8,42 +8,43 @@ def get_prompt(background, situation, questions, answers):
 
     return f"""Given the following information, answer the questions below:
 {background} {situation}
-Q: {questions[0]}
-A: {answers[0]}
-Q: {questions[1]} 
-A: {answers[1]}
-A: {answers[2]}
-Q: {questions[3]}
-A:"""
+Question: {questions[0]}
+Answer: {answers[0]}
+Question: {questions[1]} 
+Answer: {answers[1]}
+Answer: {answers[2]}
+Question: {questions[3]}
+Answer:"""
 
 ropes = json.loads(open("validation_condensed.json", 'r').read()) 
-print(ropes[20])
 
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neox-20b")
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")
+tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom", device_map='auto', torch_dtype='auto')
 
 for i, r in enumerate(tqdm(ropes)):
-    if i == 10:
-        break
-    background, situation, questions, answers = r.values()
-    prompt = get_prompt(background, situation, questions, answers)
-    
-    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+	if i == 1:
+		break
+	background, situation, questions, answers = r.values()
+	prompt = get_prompt(background, situation, questions, answers)
 
-    gen_tokens = model.generate(
-        input_ids,
-        pad_token_id=tokenizer.eos_token_id,
-        do_sample=True,
-        temperature=0.33,
-        max_length=input_ids.shape[1]+5)
+	print("Tokenizing prompt: \n{prompt}")
+	input_ids = tokenizer(prompt, return_tensors="pt").input_ids
 
-    y_hat = tokenizer.batch_decode(gen_tokens)[0]
-    print(f"{y_hat=}")
-    pred_answers.append(f"{prompt}:{y_hat} CORRECT:{answers}\n")
+	print("Feeding to model...")
+	gen_tokens = model.generate(
+		input_ids,
+		pad_token_id=tokenizer.eos_token_id,
+		do_sample=True,
+		temperature=0.33,
+		max_length=input_ids.shape[1]+5)
+
+	y_hat = tokenizer.batch_decode(gen_tokens)[0]
+	print(f"{y_hat=}")
+	pred_answers.append(f"{prompt}:{y_hat} CORRECT:{answers}\n")
 
 out = f"few_shot_gpt-neox-20b_answers.txt"
 try:
-    with open(out, 'w') as f:
-        f.writelines(answers)
+	with open(out, 'w') as f:
+		f.writelines(answers)
 except IOError:
-    print("I/O error")
+	print("I/O error")
